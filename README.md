52bfe4d7b21cccdc0af9378a31cd29eaece25cec
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2024-09-09 17:56:07 +0700

    restaurant project finished

---
+++ README.md
@@ -0,0 +1,1 @@
+Basic restaurant page for Odin Project
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const { watchFile } = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html'
+        })
+    ],
+    devtool: 'eval-source-map',
+    devServer: {
+        watchFiles: ['./src/template.html']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(jpg|svg|png|gif|jpeg)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    }
+}
